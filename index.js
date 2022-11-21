99a73683107b7bddb66fe48c921bd22d99c960f8
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2022-11-21 14:46:27 +0700

    update js and css
    reduced the number of font options, updated JS to make the code cleaner

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+/* Passenger Counter App */
+
 let count = 0
 
 const countEl = document.getElementById("count-el");
 
 function increment() {
-    count = count + 1;
-    countEl.innerText = count;
+    count += 1;
+    countEl.textContent = count;
 };
 
 function decrease() {
   if (count != 0) {
-    count = count - 1;
-    countEl.innerText = count;
+    count -= 1;
+    countEl.textContent = count;
 
   }
 
@@ -18,5 +20,5 @@ function decrease() {
 
 function reset() {
   count = 0;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
